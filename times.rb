4a3aec0cbd3537e1c59ef9feb67fe9900df6c109
armandylon <dire26463@gmail.com>
2016-03-15 13:26:52 +0000

    nitrous-backup at Tue Mar 15 13:26:52 UTC 2016

--- times.rb
+++ times.rb
@@ -1,5 +1,12 @@
-def using_times
+def using_times()
+
+end
 	#your code here
+ looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+
+7.times do
+  using_times.looping_string
+  puts  "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
 end
 
 
